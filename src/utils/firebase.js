c1fccad4fb7779d1aac7c7bc4b7f9224e3891bee
gufran0 <md.gufran@techchefz.com>
2024-06-09 01:20:52 +0530

    firebase initilization added

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,20 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD3sjfUb_aOZ5x0cmBNdmDE1W-j5osuCfM",
-  authDomain: "ngpt-ba991.firebaseapp.com",
-  projectId: "ngpt-ba991",
-  storageBucket: "ngpt-ba991.appspot.com",
-  messagingSenderId: "272062829525",
-  appId: "1:272062829525:web:a39e043351e0ba387f235c",
-  measurementId: "G-V0X2ENBNQ3",
+  apiKey: "AIzaSyDs-KFTHpp8_M_A-zPQ5iwjVShO2HQVTQ0",
+  authDomain: "ngpt-new.firebaseapp.com",
+  projectId: "ngpt-new",
+  storageBucket: "ngpt-new.appspot.com",
+  messagingSenderId: "73557690745",
+  appId: "1:73557690745:web:4c67868cf6a768ca5007a8",
+  measurementId: "G-4PG3QV7GFM"
 };
 
 // Initialize Firebase
